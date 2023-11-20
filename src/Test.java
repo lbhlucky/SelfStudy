02ce119d19335777d1fdcb060ea0eda1180d4e01
lbhlucky <69577337+lbhlucky@users.noreply.github.com>
2023-11-20 19:01:51 +0900

    23.11.20_vsc_change

--- src/Test.java
+++ src/Test.java
@@ -15,7 +15,7 @@ public class Test {
         Test test = new Test();
         System.out.println(test.factorial(n));
 
-        System.out.println("VSC TEST");
-
+            System.out.println("VSC TEST");
+           1ㅣ l I i j J
     }
 }
