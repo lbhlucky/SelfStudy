ac8ae388f14c44d5add9b565cdd68e18131c6db9
lbhlucky <69577337+lbhlucky@users.noreply.github.com>
2023-11-27 17:02:06 +0900

    test1

--- src/Binary_Search/TwoSolution.java
+++ src/Binary_Search/TwoSolution.java
@@ -159,7 +159,7 @@ public class TwoSolution {
     }
 
     public static void main(String[] args) {
-        input();
-        pro();
+        input();    // 입력 받기
+        pro();      // 계산 후 출력
     }   
 }
